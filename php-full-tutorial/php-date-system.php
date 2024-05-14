0f29ab7cd7efa4e4c2eef54bbb845d1c8e38a668
nithushan mohan <nithushan014@gmail.com>
2024-05-14 11:08:18 +0530

    Feat: Update some files and some codes line

---
+++ php-full-tutorial/php-date-system.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Today Date</title>
+</head>
+<style>
+h1{
+    color: blue;
+    font-size: 35px;
+    text-align: center;
+}
+body{
+    background-color: yellow;
+}
+echo{
+    
+}
+</style>
+<body>
+    
+<h1>Today Date</h1>
+
+<?php
+echo "Today is " . date("Y-m-d") . "<br>","<br>";
+echo "Today is " . date("l");
+?>
+
+
+</body>
+</html>
---
+++ php-full-tutorial/php-include.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<html>
+<body>
+
+<h1>Welcome to my home page!</h1>
+
+<?php include 'noFileExists.php';
+echo "I have a $color $car.";
+?>
+
+</body>
+</html>
+</body>
+</html>
